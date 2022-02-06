8053175e672a7d35655100265c18648a370b9453
Asmaa Outaleb <asmaaoutaleb1@gmail.com>
2022-02-06 19:54:22 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorsComponent } from './authors/authors.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CreateItemComponent } from './create-item/create-item.component';
+import { FaqComponent } from './faq/faq.component';
+import { HelpCenterComponent } from './help-center/help-center.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { WalletConnectComponent } from './wallet-connect/wallet-connect.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'transactions', component: TransactionComponent},
+  {path: 'authors', component: AuthorsComponent},
+  {path: 'help', component: HelpCenterComponent},
+  {path: 'contact', component: ContactUsComponent},
+  {path: 'faq', component: FaqComponent},
+  {path: 'wallet', component: WalletConnectComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'createItem', component: CreateItemComponent},
+  {path: 'profileDetails', component: ProfileDetailsComponent},
+  { path: 'user', component: BoardUserComponent },
+  { path: 'admin', component: BoardAdminComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,64 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';//pour gerer les requetes http
+import { AppRoutingModule } from './app-routing.module';//pour gerer les formulaires
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AnnonceComponent } from './annonce/annonce.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { CategoryComponent } from './category/category.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CreateItemComponent } from './create-item/create-item.component';
+import { FaqComponent } from './faq/faq.component';
+import { FooterComponent } from './footer/footer.component';
+import { HelpCenterComponent } from './help-center/help-center.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { OperationComponent } from './operation/operation.component';
+import { ProductComponent } from './product/product.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { SignupComponent } from './signup/signup.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { WalletConnectComponent } from './wallet-connect/wallet-connect.component';
+import { authInterceptorProviders } from './_helpers/auth.interceptor';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardUserComponent } from './board-user/board-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AnnonceComponent,
+    AuthorsComponent,
+    CategoryComponent,
+    ContactUsComponent,
+    CreateItemComponent,
+    BoardAdminComponent,
+    BoardUserComponent,
+    FaqComponent,
+    FooterComponent,
+    HelpCenterComponent,
+    HomeComponent,
+    LoginComponent,
+    OperationComponent,
+    ProductComponent,
+    ProfileComponent,
+    ProfileDetailsComponent,
+    SignupComponent,
+    TransactionComponent,
+    WalletConnectComponent,
+    BoardAdminComponent,
+    BoardUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
